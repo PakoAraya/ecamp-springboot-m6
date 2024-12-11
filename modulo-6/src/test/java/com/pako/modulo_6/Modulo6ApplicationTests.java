80560c742b9c14f8add1d4e640c162381530be4c
franarayah <franarayah@gmail.com>
2024-12-11 19:08:20 -0300

    Se establece la base del repositorio

---
+++ modulo-6/src/test/java/com/pako/modulo_6/Modulo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pako.modulo_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
